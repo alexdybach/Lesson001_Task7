fcf1b01432e1fee2edece4bd869e04ac363cca99
Олександр Дибач <alex.dybach.2014@i.ua>
2020-10-27 13:46:10 +0200

    Done lesson 1 task 7

---
+++ Lesson001_Task7/Lesson001_Task7/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson001_Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char gender = 'M';
+            Console.WriteLine("Gender: {0}",gender);
+        }
+    }
+}
